2507dc1899ac2f6255609cfe66f139c4ad204ec4
Steven Te <testeven152@gmail.com>
2018-11-29 19:29:41 -0800

    psid update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//webhook from facebook
 var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
@@ -22,4 +23,53 @@ app.get("/webhook", function (req, res) {
     console.error("Verification failed. The tokens do not match.");
     res.sendStatus(403);
   }
+
+  
 });
+
+app.post('/webhook', (req, res) => {  
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Get the webhook event. entry.messaging is an array, but 
+      // will only ever contain one event, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+        // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+      
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}

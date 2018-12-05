987864d4eb0a9ab0316cdd13703272786b634436
Steven Te <testeven152@gmail.com>
2018-12-04 17:08:38 -0800

    sendapi update

--- index.js
+++ index.js
@@ -92,11 +92,25 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-   // Construct the message body
-   let request_body = {
+  // Construct the message body
+  let request_body = {
     "recipient": {
       "id": sender_psid
     },
     "message": response
   }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
 }
